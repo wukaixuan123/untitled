781d1aaa895e093c0e38ad78beab40f7d8a46248
wuakaixuan <1329267787@qq.com>
2021-07-10 23:10:49 +0800

    cts

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,8 +5,9 @@ public class Main {
     public static void main(String[] args) {
 	// write your code here
         System.out.println("你好啊哦");
+        a();
     }
-	public void a(){
+	public static void a(){
 	System.out.println("第一次修改");
 	}
 }
